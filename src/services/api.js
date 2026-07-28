296383499e09dfae8eddb131a337d10aae663a35
QueenNhi <hothinhikare@gmail.com>
2026-07-29 00:21:54 +0700

    fix: update api config with import.meta.env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 // src/services/api.js
 import axios from 'axios';
-axios.create({
-    baseURL: "https://ecommerce-project-n45y.onrender.com/api"
-})
 
-export default API_URL;
+const API = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || "https://ecommerce-project-n45y.onrender.com/api"
+});
+
+export default API;
